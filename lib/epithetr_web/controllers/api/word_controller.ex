8b911cefb5bedce73c88e82b291506a8d3031110
Josianne Hyson <josianne.hyson@gmail.com>
2017-10-27 12:50:28 +1300

    Return many nouns and adjectives in the API call for words

--- lib/epithetr_web/controllers/api/word_controller.ex
+++ lib/epithetr_web/controllers/api/word_controller.ex
@@ -5,7 +5,8 @@ defmodule EpithetrWeb.Api.WordController do
 
   def index(conn, _params) do
     adjectives = Words.list_adjectives()
+    nouns = Words.list_nouns()
 
-    render conn, "index.json", adjectives: adjectives
+    render conn, "index.json", adjectives: adjectives, nouns: nouns
   end
 end
--- lib/epithetr_web/views/api/word_view.ex
+++ lib/epithetr_web/views/api/word_view.ex
@@ -1,11 +1,14 @@
 defmodule EpithetrWeb.Api.WordView do
   use EpithetrWeb, :view
 
-  def render("index.json", %{adjectives: adjectives}) do
-    %{data: render_many(adjectives, EpithetrWeb.Api.WordView, "word.json")}
+  def render("index.json", %{adjectives: adjectives, nouns: nouns}) do
+    %{
+      adjectives: render_many(adjectives, EpithetrWeb.Api.WordView, "word.json"),
+      nouns: render_many(nouns, EpithetrWeb.Api.WordView, "word.json"),
+    }
   end
 
   def render("word.json", %{word: word}) do
-    %{word: word.word}
+    %{word: String.downcase(word.word)}
   end
 end
