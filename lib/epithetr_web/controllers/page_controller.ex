2ada52841d3f45d0c28f3f21cb35e1bd9011e9bc
Josianne Hyson <josianne.hyson@gmail.com>
2017-10-31 18:19:23 +1300

    Some style updates

--- lib/epithetr_web/controllers/page_controller.ex
+++ lib/epithetr_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule EpithetrWeb.PageController do
   use EpithetrWeb, :controller
 
+  alias Epithetr.Words
+  alias Epithetr.Words.Noun
+  alias Epithetr.Words.Adjective
+
   def index(conn, _params) do
-    render conn, "index.html"
+    nouns = Words.list_nouns()
+    adjectives = Words.list_adjectives()
+    render(conn, "index.html", nouns: nouns, adjectives: adjectives)
   end
 end
