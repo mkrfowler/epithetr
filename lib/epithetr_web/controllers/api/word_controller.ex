d48502b1f847e0e6150bf537580502e3dd05cc20
Josianne Hyson <josianne.hyson@gmail.com>
2017-11-17 21:54:32 +1300

    Fix issues...

--- lib/epithetr_web/controllers/api/word_controller.ex
+++ lib/epithetr_web/controllers/api/word_controller.ex
@@ -10,9 +10,9 @@ defmodule EpithetrWeb.Api.WordController do
     render conn, "index.json", adjectives: adjectives, nouns: nouns
   end
 
-  def unwholesome_index(conn, _params) do
-    adjectives = Words.random_adjectives(1)
-    nouns = Words.random_nouns(1)
+  def unwholesome_index(conn, params) do
+    adjectives = Words.random_adjectives(params["adjectives"] || 1)
+    nouns = Words.random_nouns(params["nouns"] || 1)
 
     render conn, "index.json", adjectives: adjectives, nouns: nouns
   end
